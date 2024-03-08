666e38aa65dc92c5346ad6d3a5197136c1c6be51
soniya <soniyasshivam10@gmail.com>
2024-03-08 11:47:43 +0530

    new

---
+++ ques.js
@@ -0,0 +1,122 @@
+let questions = [
+    {
+        numb : 1,
+        question: "What is the capital city of Australia?",
+        answer:"Canberra",
+        options:[
+            "Melbourne",
+            "Sydney" ,
+            "Canberra" ,
+            "Brisbane" 
+        ]
+        
+    },
+    {
+        numb : 2,
+        question: "Which planet is known as the Red Planet?",
+        answer:"Mars",
+        options:[
+            "Venus",
+            "Mars",
+            "Jupiter",
+            "Saturn"
+        ]
+        
+    },
+    {
+        numb : 3,
+        question: "Who is known as the father of modern physics?",
+        answer:"Isaac Newton",
+        options:[
+             "Isaac Newton" ,
+             "Galileo Galilei" ,
+             "Albert Einstein" ,
+             "Nikola Tesla" 
+        ]
+        
+    },
+    {
+        numb : 4,
+        question: "What is the chemical symbol for gold?",
+        answer:"Au",
+        options:[
+            "Ag",
+            "Au",
+            "Fe",
+            "Cu" 
+        ]
+        
+    }, 
+    {
+        numb : 5,
+        question: "In which year did the Titanic sink?",
+        answer:"1912",
+        options:[
+            "1920",
+            "1915" ,
+            "1908" ,
+            "1912" 
+        ]
+        
+    },
+    {
+        numb : 6,
+        question: "Who painted the Mona Lisa?",
+        answer:"Leonardo da Vinci",
+        options:[
+            "Vincent van Gogh",
+            "Pablo Picasso" ,
+            "Leonardo da Vinci" ,
+            "Michelangelo" 
+        ]
+        
+    },
+    {
+        numb : 7,
+        question: "Which country is known as the Land of the Rising Sun?",
+        answer:"Japan",
+        options:[
+            "China",
+            "Japan" ,
+            "South Korea" ,
+            "Thailand" 
+        ]
+        
+    },
+    {
+        numb : 8,
+        question: "What is the longest river in the world?",
+        answer:"Nile",
+        options:[
+            "Nile",
+            "Amazon" ,
+            "Mississippi" ,
+            "Yangtze" 
+        ]
+        
+    },
+    {
+        numb : 9,
+        question: "Who wrote the famous novel -To Kill a Mockingbird?",
+        answer:"Harper Lee",
+        options:[
+            "Ernest Hemingway",
+            "Harper Lee" ,
+            "F. Scott Fitzgerald" ,
+            "Mark Twain" 
+        ]
+        
+    },
+    {
+        numb : 10,
+        question: "Who wrote the famous play Romeo and Juliet?",
+        answer:"William Shakespeare",
+        options:[
+            "William Shakespeare",
+            "John Milton" ,
+            "Geoffrey Chaucer" ,
+            "Charles Dickens" 
+        ]
+        
+    },
+]
